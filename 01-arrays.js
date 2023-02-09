588f5e5b1967f2791c2ec4cfc9c4b5dd8c0ffee9
Camilo Franco Moya <69817111+Aleksei324@users.noreply.github.com>
2023-02-09 01:15:23 -0500

    updating

--- 01-arrays.js
+++ 01-arrays.js
@@ -2,17 +2,15 @@
 https://www.w3schools.com/jsref/jsref_obj_array.asp
 */
 
-const array1 = ['Alan', 'Bryan', 'Carl'];
-const array2 = ['Orion', 'Kyle', 'Rey'];
+const array1 = ["Alan", "Bryan", "Carl"];
+const array2 = ["Orion", "Kyle", "Rey"];
 
 // I will be printing a lot so this is easier
 const p = (x) => console.log(x);
 
-
 p(`Array 1: ${array1}
 Array 2: ${array2}
 ----- < 1 - 5 > -----`);
-
 
 // 1
 const array3 = [...array1.concat(array2)];
@@ -27,33 +25,31 @@ p(array3); // mutado
 // 3
 // entries() regresa un iterable con llaves y valores
 for (const [key, value] of array1.entries()) {
-	p(`llave: ${key}; valor: ${value}`);
+  p(`llave: ${key}; valor: ${value}`);
 }
 
 // 4
 // revisa si el array pasa un test
-p(array1.every(x => x !== 'Juan'));
+p(array1.every((x) => x !== "Juan"));
 
 // 5
 // llena el array con el valor
-array1.fill('Hall', 0, 2);
+array1.fill("Hall", 0, 2);
 p(array1); // mutado
 
-
-p('----- < 6 - 10 > -----');
-
+p("----- < 6 - 10 > -----");
 
 // 6
 // crea un nuevo array con los valores que cumplan el test
-p(array1.filter(x => x === 'Hall'));
+p(array1.filter((x) => x === "Hall"));
 
 // 7
 // regresa el valor de quien cumpla el test
-p(array2.find(x => x.charAt(0) === 'K'));
+p(array2.find((x) => x.charAt(0) === "K"));
 
 // 8
 // regresa el index de quien cumpla el test
-p(array3.findIndex(x => x.charAt(2) === 'y'));
+p(array3.findIndex((x) => x.charAt(2) === "y"));
 
 // 9
 // runs a function for each element
@@ -61,50 +57,46 @@ array2.forEach(p);
 
 // 10
 // creates an array from something
-p(Array.from('ABCDE'));
+p(Array.from("ABCDE"));
 
-
-p('----- < 11 - 15 > -----');
-
+p("----- < 11 - 15 > -----");
 
 // 11
 // check if there is that element
-p(array1.includes('Hall'));
+p(array1.includes("Hall"));
 
 // 12
 // return the index of the element
-p(array2.indexOf('Orion'));
+p(array2.indexOf("Orion"));
 
 // 13
 p(Array.isArray(array1));
 
 // 14
 // creates a string with all elements and a separator
-p(array3.join('-'));
+p(array3.join("-"));
 
 // 15
 // crea un iterable con las llaves
 for (const key of array2.keys()) {
-	p(`llave: ${key}`);
+  p(`llave: ${key}`);
 }
 
-
-p('----- < 16 - 20 > -----');
-
+p("----- < 16 - 20 > -----");
 
 // 16
-p(array2.lastIndexOf('Orion'));
+p(array2.lastIndexOf("Orion"));
 
 // 17
 // crea un nuevo array donde cada elemento del anterior array es modificado por la función y retornado
-p(array2.map(x => x + ' del Rosario'));
+p(array2.map((x) => x + " del Rosario"));
 
 // 18
 p(array3.pop());
 p(array3); // mutado
 
 // 19
-p(array1.push('Orion'));
+p(array1.push("Orion"));
 p(array1); // mutado
 
 // 20
@@ -113,9 +105,7 @@ p(array1); // mutado
 let n = [1, 4, 10, 22, 46];
 p(n.reduce((prev, current) => prev + current));
 
-
-p('----- < 21 - 25 > -----');
-
+p("----- < 21 - 25 > -----");
 
 // 21
 p(n.reduceRight((prev, current) => prev - current));
@@ -135,11 +125,9 @@ p(array3.slice(1, 3));
 
 // 25
 // check if any element apply
-p(array1.some((x) => x === 'Orion'));
+p(array1.some((x) => x === "Orion"));
 
-
-p('----- < 26 - 30 > -----');
-
+p("----- < 26 - 30 > -----");
 
 // 26
 array1.sort();
@@ -155,13 +143,12 @@ p(array1.toString());
 
 // 29
 // inserta elemento al inicio
-array3.unshift('Bob');
+array3.unshift("Bob");
 p(array3); // mutado
 
 // 30
 // return itself
 p(array1.valueOf());
-
 
 p(`----- -----
 Array 1: ${array1}
